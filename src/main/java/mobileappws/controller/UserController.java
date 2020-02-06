2bcd7a0ffa857a6b8e1f8a3c35ec3da1d802ba1e
Douglas J. Costa <douglas.costa@squadra.com.br>
2020-02-06 15:37:55 -0300

    Creating application controller

--- src/main/java/mobileappws/controller/UserController.java
+++ src/main/java/mobileappws/controller/UserController.java
@@ -1,33 +1,62 @@
 
 package mobileappws.controller;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
- 
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import mobileappws.Dtos.UserRestDTO;
+import mobileappws.model.UserRest;
+
+
 @RestController
 @RequestMapping("users")
 public class UserController {
 
-    @GetMapping
-    public String getUser() {
-        return "Get user was called";
+    @GetMapping(path = "/{userId}",
+                produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<UserRest> getUserById(@PathVariable String userId) {
+        UserRest returnValue = new UserRest();
+
+        returnValue.setFirstName("teste");
+        returnValue.setLastName("teste2");
+        returnValue.setEmail("teste@gmail.com");
+
+        return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
     }
 
-    @PostMapping
-    public String createUser() {
-        //TODO: process POST request
-        
-        return "create user was called";
+    @GetMapping
+    public String getUser(@RequestParam(value = "page", defaultValue = "1") int page,
+                          @RequestParam(value = "limit", defaultValue = "50") int limit) {    
+        return "Get user was called with page " + page + "and limit: " + limit;
     }
-    
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UserRest> createUser(@RequestBody UserRestDTO userRestDTO) {
+        // TODO: process POST request
+
+        UserRest returnValue = new UserRest();
+
+        returnValue.setFirstName(userRestDTO.getFirstName());
+        returnValue.setLastName(userRestDTO.getLastName());
+        returnValue.setEmail(userRestDTO.getEmail());
+
+        return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
+    }
+
     @PutMapping
     public String putMethodName() {
-        //TODO: process PUT request
-        
+        // TODO: process PUT request
+
         return "update user was called";
     }
 
